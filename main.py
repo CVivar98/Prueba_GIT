c9af1d24a3ac5b860fb80144e1cd54a0720abdf3
KEVIN JOSE SANDOVAL CATALAN <2819606230104@ingenieria.usac.edu.gt>
2020-12-02 23:25:48 -0600

    Nuevo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Singleton import Singleton
 
 
-#Singleton.getInstance().saludar("hola que hace ?")
+Singleton.getInstance().saludar("hola que hace ?")
 
-Singleton.getInstance().ayuda("hola ")
+print("HOla desde Kevin")
